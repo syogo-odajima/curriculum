538b3ed9f32ac7d3b80556aa7936666b1815c7a1
小田嶋翔吾 <dajimatton@gmail.com>
2020-07-05 17:35:59 +0900

    2-10課題
    関数

---
+++ 2-10/index.php
@@ -0,0 +1,25 @@
+<?php
+function getTriangleArea($base, $height) {
+    $area = $base * $height / 2;
+    print "三角形の面積は".$area."だよ。";
+}
+getTriangleArea(10,5);
+
+
+function a($base,$widit,$height) {
+    $area = $base * $widit * $height;
+    print "直方体の体積は".$area."です";
+}
+ a(5,10,8); 
+echo "<br>";
+ echo "<br>";
+ echo "IT用語"."<br>";
+ echo "・ハッシュ化"."<br>";
+ echo "ハッシュ関数と呼ばれてる「値を中に放り込む適当な値に入れてハッシュ値と呼ばれる適当な値に変換すること"."<br>";
+ echo "・統合テスト"."<br>";
+ echo "情報システムやソフトウェアのテスト手法の一つで、完成したシステムが外部の他のシステムなどと期待通りに連携、接続して動作することを確認するテスト"."<br>";
+ echo "・デバッグ"."<br>";
+ echo "バグの原因を探して取り除く作業のこと"."<br>";
+
+?>
+
