dfa2ea79f2b6502c09878dc61c383f68ec2334d1
小田嶋翔吾 <dajimatton@gmail.com>
2020-07-12 11:37:47 +0900

    2-11課題
    フォームに入力した内容を入力する

---
+++ 2-11/index.php
@@ -0,0 +1,19 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="my_name" />
+  <br>
+  メールアドレス：<input type="email" name="email" />
+  <br>
+
+  パスワード：<input type="password" name="password" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
+<?php
+echo "IT用語"."<br>";
+echo "1.仕様書、設計書"."<br>";
+echo "仕様書とは　使用をまとめた資料のこと。設計書とは　設計した内容をまとめたもの"."<br>";
+echo "2.git"."<br>";
+echo "gitとはバージョン管理システムの一つ。ファイル履歴を変更してくれるシステムの一つ"."<br>";
+echo "3.マージツール"."<br>";
+echo "マージツールとは、バージョン管理システムの一つ。"."<br>";
+?>
