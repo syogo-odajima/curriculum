73c9f7b196a4188a566b3e7769bac172c198ca57
小田嶋翔吾 <dajimatton@gmail.com>
2020-07-15 23:55:01 +0900

    Revert "課題提出テスト"
    This reverts commit 4a2b15a0d90b71f8ca3c3a32d4fbcf430239d22c.

--- 2-12/result.php
+++
@@ -1,8 +0,0 @@
-<?php
-$my_name = $_POST['my_name'];
-$rank = $_POST['rank'];
-$num = $_POST['num'];
-?>
-<p>私の名前<?php echo $_my_name; ?>です。</p>
-<p>ご希望商品<?php echo $_rank; ?>=です。</p>
-<p>個数<?php echo $_my_name; ?>=です。</p>
