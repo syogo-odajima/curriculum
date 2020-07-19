7606b62821839aef95721e18953dee539257f295
小田嶋翔吾 <dajimatton@gmail.com>
2020-07-19 18:27:46 +0900

    2-15課題
    便利な関数（日付・時間）

---
+++ 2-15/index.php
@@ -0,0 +1,48 @@
+<!-- <?php
+echo time();
+?>
+<br>
+
+<?php
+var_dump(time());
+?>
+<br>
+
+<?php
+echo date("Y-m-d H:i:s",time());
+?>
+<br>
+
+<?php
+echo strtotime("2020/7/19 16:57:00");
+?>
+<br>
+
+<?php
+echo strtotime("last sunday")."<br>";
+echo strtotime("+2 day");
+?>
+<br>
+<br> -->
+
+
+<?php
+echo "課題"."<br>";
+echo strtotime("2017/12/31 23:59:59")."<br>";
+echo date("Y年m月d日 H時i分s秒", 1514764799)."<br>";
+
+echo "IT用語"."<br>";
+echo "ログとは、コンピュータがつけている記録のようなもの"."<br>";
+echo "ログの確認の仕方は、コマンドプロンプトを開き「eventvwr.msc」と入力します。 別ウィンドウが立ち上がりますので、左メニューの「Windowsログ」→「システム」を選択します。 画面に中央部分にシステムログが表示されますので、こちらで起動・シャットダウンのログを確認する事が可能です。"."<br>";
+
+echo "クラウドサーバーとは、ネットワークに通じていつでもどこでも簡単にアプリケーション、サーバ、ストレージ、サービスなどにアクセスできる"."<br>";
+echo "レンタルサーバーとは、共有してサーバとして使われることが多い"."<br>";
+
+echo "VPSとは仮想専用サーバーのこと"."<br>";
+
+echo "PHPのFWの種類は１０種類以上ある。例(CakePHP、Symfony、CodeIgniter、ZendFramework、Phalcon、) "."<br>";
+echo "PHPのFWの特徴"."<br>";
+echo "CakePHPとはケーキを焼く様に簡単にPHPで開発ができるフレームワーク”を目指して開発されたフレームワークです。"."<br>";
+echo "Symfonyとは、CakePHPとの違いは、CakePHPが誰でも簡単に開発できることを目標にした開発者目線で作られたフレームワークなのに対して、Symfonyは企業の重要なシステム開発を担うことができること目指した、成果物に着眼したフレームワークです。"."<br>";
+echo "CodeIgniterとは、CakePHPにしてもSymfonyにしても、誰もがある程度の品質のソースを書けることを意識したフレームワークだったため、コーディング規約にうるさいという特徴があります。"."<br>";
+echo "ZendFrameworkとは、2006年に登場しましたが、拡張性が高く、特に大規模な開発で強みを発揮します。"."<br>";
