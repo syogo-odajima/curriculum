62097ee153949fee66e2a36b784000ffb6b0614d
小田嶋翔吾 <dajimatton@gmail.com>
2020-08-02 16:41:09 +0900

    2-2課題
    変数とデータ型

---
+++ 2-2/index.php
@@ -0,0 +1,18 @@
+<?php
+$message = "Hello world!";
+echo $message . "<br>";
+var_dump($message) . "<br>";
+
+$emp = null;
+var_dump($emp) . "<br>";
+
+$x = 8;
+$y = 5.55;
+echo $x;
+echo $y . "<br>";
+
+"<br>";
+
+echo "IT用語" . "<br>";
+echo "APIとは、ある一つの機能に特化したプログラムで共有可能なものソフトウェアの機能を共有する仕組み" . "<br>";
+echo "SNVとは、オープンソースの集中型バージョン管理システム" . "<br>";
