94b41d4d0d1eeebc6b2a31bd94c2e24bf83e6e3e
小田嶋翔吾 <dajimatton@gmail.com>
2020-08-02 16:43:23 +0900

    2-6課題
    ループ文

---
+++ 2-6/index.php
@@ -0,0 +1,18 @@
+<?php
+$num = 1;
+while($num<101){
+    echo $num;
+    echo "<br>";
+    $num++;
+}
+
+echo "IT用語" ."<br>";
+echo "サブクエリ" . "<br>";
+echo "QL文の結果を使ったSQL文にしたり、SQL文を入れ子にしたSQL文における中に書いてある方のSQL文のこと" . "<br>";
+echo "Bootstra" . "<br>";
+echo "HTML.CSS.JavaScriptから構成される最も有名WEBフレームワーク" . "<br>";
+echo "フレームワーク" . "<br>";
+echo "汎用的に必要なものをまとめている枠組み" . "<br>";
+echo "ライブラリ" . "<br>";
+echo "汎用性高い便利なプログラムのまとまり" . "<br>";
+
