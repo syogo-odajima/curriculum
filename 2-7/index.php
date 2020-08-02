3c58860970aa45313fab90040a11c6e2a06a09b2
小田嶋翔吾 <dajimatton@gmail.com>
2020-08-02 16:44:02 +0900

    2-7課題
    配列、連想配列

---
+++ 2-7/index.php
@@ -0,0 +1,19 @@
+<?php
+$color = ["red" => "赤","blue" =>"青","green" => "緑"];
+
+var_dump($color);
+echo "<br>";
+$color["yellow"] = "黄色";
+var_dump($color);
+echo"<br>"
+?><br>
+
+<?php
+//IT用語
+echo "プルリクエスト"."<br>";
+echo "コードの変更をレビュワーで通知してマージを依頼する機能です"."<br>";
+echo "git Flow"."<br>";
+echo "GITを使う際に役立つ運用ルールをまとめたツール"."<br>";
+echo "CRON"."<br>";
+echo "UNIX系システムで使用されているジョブを自動実行するスケジューラー"."<br>";
+?>
