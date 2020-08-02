94c4ba32458f40f9762d3277fbfa68856fd736c2
小田嶋翔吾 <dajimatton@gmail.com>
2020-08-02 16:41:41 +0900

    2-3
    定数

---
+++ 2-3/index.php
@@ -0,0 +1,14 @@
+<?php
+define("ADMIN_EMAIL", "y-i-group@gmail.com");
+define("LIST_COUNT", 15);
+
+echo ADMIN_EMAIL;
+echo '<br>';
+echo LIST_COUNT . "<br>"; 
+echo "<br>";
+
+echo "IT用語" . "<br>";
+echo "デプロイとは、作ったプログラムをしかるべき場所に置いたり、色々な設定をして使える状態にする事" . "<br>";
+echo "バリデーションとは、入力チェックの事" . "<br>";
+echo "アジャイルとは、作りながら考える開発方式" . "<br>";
+echo "ウォーターフォールとは、工程を一つずつ順番に終わらせていき、後戻りをしないやり方" . "<br>";
